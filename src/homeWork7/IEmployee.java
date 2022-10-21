fb85aa3bd67e798718f8dbfedc4a3e1838c531e7
ArtemChumachenko <chumachenko.artem@icloud.com>
2022-10-21 01:05:59 -0400

    Employee

---
+++ src/homeWork7/IEmployee.java
@@ -0,0 +1,6 @@
+package homeWork7;
+
+public interface IEmployee {
+
+    int getSalary(Month[] arr);
+}
---
+++ src/homeWork7/Manager.java
@@ -0,0 +1,44 @@
+package homeWork7;
+
+public class Manager extends BaseEmployee implements IManager {
+    private int numberOfSubordinates;
+
+    public Manager(String name, int age, char sex, int salaryPerDay, int numberOfSubordinates) {
+        super (name, age, sex, salaryPerDay);
+        this.numberOfSubordinates = numberOfSubordinates;
+    }
+
+    @Override
+    public int getNumberOfSubordinates() {
+        return numberOfSubordinates;
+    }
+
+    public void setNumberOfSubordinates(int numberOfSubordinates) {
+        this.numberOfSubordinates = numberOfSubordinates;
+    }
+
+
+
+    @Override
+    public int getSalary(Month[] arr) {
+        int salary = 0;
+        for (Month month : arr) { //Для всех значений month которые имеют тип Month, из массива arr делаем следующее:
+            if (numberOfSubordinates == 0) {
+                return getSalaryPerDay() * month.getAmountOfWorkDays() * SALARY_COEFFICIENT;
+            } else {
+                salary += getSalaryPerDay() * month.getAmountOfWorkDays() * numberOfSubordinates / 100 * SALARY_COEFFICIENT;
+            }
+        }
+        return salary;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Manager " + getName () + " is " + getAge () + " years old" +
+                " has " + numberOfSubordinates + " number of subordinates ";
+    }
+
+
+}
